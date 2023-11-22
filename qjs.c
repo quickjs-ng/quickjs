780efd3c7f2ea14570f4bfb6ac0b4c9c2f98807c
Saúl Ibarra Corretgé <s@saghul.net>
2023-11-22 23:52:16 +0100

    Add -ng suffix to CLI output

--- qjs.c
+++ qjs.c
@@ -269,7 +269,7 @@ static const JSMallocFunctions trace_mf = {
 
 void help(void)
 {
-    printf("QuickJS version %s\n"
+    printf("QuickJS-ng version %s\n"
            "usage: " PROG_NAME " [options] [file [args]]\n"
            "-h  --help         list options\n"
            "-e  --eval EXPR    evaluate EXPR\n"
