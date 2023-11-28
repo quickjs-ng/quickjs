quickjs-ng/quickjs

commit 8be0358dd7b767e6665effb6157a77f0ab0de514

Ignore testdir config when -d or -f is passed (#145)
Otherwise it runs the whole test262 suite even though I just want to run
a select few.

run-test262.c
@@ -911,7 +911,7 @@ void update_exclude_dirs(void)
     lp->count = count;
 }
 
-void load_config(const char *filename)
+void load_config(const char *filename, const char *ignore)
 {
     char buf[1024];
     FILE *f;
@@ -964,6 +964,10 @@ void load_config(const char *filename)
         case SECTION_CONFIG:
             if (!q) {
                 printf("%s:%d: syntax error\n", filename, lineno);
+                continue;
+            }
+            if (strstr(ignore, p)) {
+                printf("%s:%d: ignoring %s=%s\n", filename, lineno, p, q);
                 continue;
             }
             if (str_equal(p, "style")) {
@@ -1981,6 +1985,8 @@ int main(int argc, char **argv)
     BOOL is_dir_list;
     BOOL only_check_errors = FALSE;
     const char *filename;
+    const char *config = NULL;
+    const char *ignore = "";
     BOOL is_test262_harness = FALSE;
     BOOL is_module = FALSE;
 
@@ -2014,14 +2020,16 @@ int main(int argc, char **argv)
         } else if (str_equal(arg, "-v")) {
             verbose++;
         } else if (str_equal(arg, "-c")) {
-            load_config(get_opt_arg(arg, argv[optind++]));
+            config = get_opt_arg(arg, argv[optind++]);
         } else if (str_equal(arg, "-d")) {
+            ignore = "testdir"; // don't run all tests, just the ones from -d
             enumerate_tests(get_opt_arg(arg, argv[optind++]));
         } else if (str_equal(arg, "-e")) {
             error_filename = get_opt_arg(arg, argv[optind++]);
         } else if (str_equal(arg, "-x")) {
             namelist_load(&exclude_list, get_opt_arg(arg, argv[optind++]));
         } else if (str_equal(arg, "-f")) {
+            ignore = "testdir"; // don't run all tests, just the one from -f
             is_dir_list = FALSE;
         } else if (str_equal(arg, "-r")) {
             report_filename = get_opt_arg(arg, argv[optind++]);
@@ -2038,6 +2046,9 @@ int main(int argc, char **argv)
             break;
         }
     }
+
+    if (config)
+        load_config(config, ignore);
 
     if (optind >= argc && !test_list.count)
         help();

commit dde92587883dc7d550591f30cb07b89a28813d98

Unbreak run-test262
Commit 8be0358dd7 broke `run-test262 -c test262.conf -a`, the delayed
parsing of the config file overwrote the effect of the `-a` flag.

run-test262.c
@@ -1985,7 +1985,6 @@ int main(int argc, char **argv)
     BOOL is_dir_list;
     BOOL only_check_errors = FALSE;
     const char *filename;
-    const char *config = NULL;
     const char *ignore = "";
     BOOL is_test262_harness = FALSE;
     BOOL is_module = FALSE;
@@ -1995,6 +1994,18 @@ int main(int argc, char **argv)
     /* Date tests assume California local time */
     setenv("TZ", "America/Los_Angeles", 1);
 #endif
+
+    optind = 1;
+    while (optind < argc) {
+        char *arg = argv[optind];
+        if (*arg != '-')
+            break;
+        optind++;
+        if (strstr("-c -d -e -x -f -r -E -T", arg))
+            optind++;
+        if (strstr("-d -f", arg))
+            ignore = "testdir"; // run only the tests from -d or -f
+    }
 
     /* cannot use getopt because we want to pass the command line to
        the script */
@@ -2020,16 +2031,14 @@ int main(int argc, char **argv)
         } else if (str_equal(arg, "-v")) {
             verbose++;
         } else if (str_equal(arg, "-c")) {
-            config = get_opt_arg(arg, argv[optind++]);
+            load_config(get_opt_arg(arg, argv[optind++]), ignore);
         } else if (str_equal(arg, "-d")) {
-            ignore = "testdir"; // don't run all tests, just the ones from -d
             enumerate_tests(get_opt_arg(arg, argv[optind++]));
         } else if (str_equal(arg, "-e")) {
             error_filename = get_opt_arg(arg, argv[optind++]);
         } else if (str_equal(arg, "-x")) {
             namelist_load(&exclude_list, get_opt_arg(arg, argv[optind++]));
         } else if (str_equal(arg, "-f")) {
-            ignore = "testdir"; // don't run all tests, just the one from -f
             is_dir_list = FALSE;
         } else if (str_equal(arg, "-r")) {
             report_filename = get_opt_arg(arg, argv[optind++]);
@@ -2046,9 +2055,6 @@ int main(int argc, char **argv)
             break;
         }
     }
-
-    if (config)
-        load_config(config, ignore);
 
     if (optind >= argc && !test_list.count)
         help();