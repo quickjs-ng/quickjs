quickjs-ng/quickjs

commit f404980ec0128e685a35fe8c9a7d746bab4ff030

Add fused get_loc0_loc1 opcode (#55)
get_loc0 and get_loc1 are individually very frequent opcodes _and_ they
are very often paired together, making them ideal candidates for opcode
fusion.

Reduces microbench.js running time by about 4%.

quickjs.c
@@ -14976,6 +14976,14 @@ static JSValue JS_CallInternal(JSContext *caller_ctx, JSValueConst func_obj,
         CASE(OP_put_loc8): set_value(ctx, &var_buf[*pc++], *--sp); BREAK;
         CASE(OP_set_loc8): set_value(ctx, &var_buf[*pc++], JS_DupValue(ctx, sp[-1])); BREAK;
 
+        // Observation: get_loc0 and get_loc1 are individually very
+        // frequent opcodes _and_ they are very often paired together,
+        // making them ideal candidates for opcode fusion.
+        CASE(OP_get_loc0_loc1):
+            *sp++ = JS_DupValue(ctx, var_buf[0]);
+            *sp++ = JS_DupValue(ctx, var_buf[1]);
+            BREAK;
+
         CASE(OP_get_loc0): *sp++ = JS_DupValue(ctx, var_buf[0]); BREAK;
         CASE(OP_get_loc1): *sp++ = JS_DupValue(ctx, var_buf[1]); BREAK;
         CASE(OP_get_loc2): *sp++ = JS_DupValue(ctx, var_buf[2]); BREAK;
@@ -29658,6 +29666,14 @@ static __exception int resolve_labels(JSContext *ctx, JSFunctionDef *s)
                     put_short_code(&bc_out, cc.op, cc.idx);
                     dbuf_putc(&bc_out, OP_add_loc);
                     dbuf_putc(&bc_out, idx);
+                    pos_next = cc.pos;
+                    break;
+                }
+                /* transformation: get_loc(0) get_loc(1) -> get_loc0_loc1 */
+                if (idx == 0 && code_match(&cc, pos_next, OP_get_loc, 1, -1)) {
+                    if (cc.line_num >= 0) line_num = cc.line_num;
+                    add_pc2line_info(s, bc_out.size, line_num);
+                    dbuf_putc(&bc_out, OP_get_loc0_loc1);
                     pos_next = cc.pos;
                     break;
                 }

quickjs-opcode.h
@@ -302,6 +302,7 @@ DEF(       get_loc8, 2, 0, 1, loc8)
 DEF(       put_loc8, 2, 1, 0, loc8)
 DEF(       set_loc8, 2, 1, 1, loc8)
 
+DEF(  get_loc0_loc1, 1, 0, 2, none_loc)
 DEF(       get_loc0, 1, 0, 1, none_loc)
 DEF(       get_loc1, 1, 0, 1, none_loc)
 DEF(       get_loc2, 1, 0, 1, none_loc)

commit b1764993dd5060315c4fd49014cc3b220a42f112

Fix OP_FMT_none_loc bytecode dumping
Commit f404980 ("Add fused get_loc0_loc1 opcode") introduced an
off-by-one (sometimes negative) array index bug because OP_get_loc1_loc1
replaced OP_get_loc0 as the first OP_FMT_none_loc opcode.

quickjs.c
@@ -27165,7 +27165,7 @@ static void dump_byte_code(JSContext *ctx, int pass,
                 printf(",%u", get_u16(tab + pos + 8));
             break;
         case OP_FMT_none_loc:
-            idx = (op - OP_get_loc0) % 4;
+            idx = (op - OP_get_loc0_loc1) % 4;
             goto has_loc;
         case OP_FMT_loc8:
             idx = get_u8(tab + pos);