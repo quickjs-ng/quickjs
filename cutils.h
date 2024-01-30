229b07b9b2c811eaf84db209a1d6f9e2a8a7b0d9
Rob Loach <robloach@gmail.com>
2024-01-30 16:46:28 -0500

    android: Additional malloc_usable_size() fixes

--- cutils.h
+++ cutils.h
@@ -40,6 +40,8 @@
 #endif
 #if defined(__APPLE__)
 #include <malloc/malloc.h>
+#elif defined(__ANDROID__)
+#include <dlmalloc.h>
 #elif defined(__linux__) || defined(__CYGWIN__)
 #include <malloc.h>
 #elif defined(__FreeBSD__)
