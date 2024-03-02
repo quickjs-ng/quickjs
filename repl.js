117ddf47adc0789c90cd24a6008e9bdbdd0e2a52
Charlie Gordon <github@chqrlie.org>
2024-03-02 15:23:42 +0100

    Improve Date.parse()
    - produce readable output for Date objects in repl

--- repl.js
+++ repl.js
@@ -938,6 +938,8 @@ import * as os from "os";
                     std.puts(a);
                 } else if (stack.indexOf(a) >= 0) {
                     std.puts("[circular]");
+                } else if (a instanceof Date) {
+                    std.puts("Date " + a.toGMTString().__quote());
                 } else {
                     stack.push(a);
                     if (Array.isArray(a)) {
