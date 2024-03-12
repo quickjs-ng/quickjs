473bd1d531ff4fc07817b9fcdd6df2857e0edb60
Saúl Ibarra Corretgé <s@saghul.net>
2024-03-12 09:06:07 +0100

    Fix Android build
    dlmalloc has been removed and the NDK now exposes a malloc.h header with
    malloc_usable_size exposed, so use that.
    
    Also remove the duplication in js__malloc_usable_size.
    
    Fixes: https://github.com/quickjs-ng/quickjs/issues/304

--- cutils.h
+++ cutils.h
@@ -38,9 +38,7 @@
 #endif
 #if defined(__APPLE__)
 #include <malloc/malloc.h>
-#elif defined(__ANDROID__)
-#include <dlmalloc.h>
-#elif defined(__linux__) || defined(__CYGWIN__)
+#elif defined(__linux__) || defined(__ANDROID__) || defined(__CYGWIN__)
 #include <malloc.h>
 #elif defined(__FreeBSD__)
 #include <malloc_np.h>
@@ -421,9 +419,7 @@ static inline size_t js__malloc_usable_size(const void *ptr)
     return malloc_size(ptr);
 #elif defined(_WIN32)
     return _msize((void *)ptr);
-#elif defined(__ANDROID__)
-    return dlmalloc_usable_size((void *)ptr);
-#elif defined(__linux__) || defined(__FreeBSD__)
+#elif defined(__linux__) || defined(__ANDROID__) || defined(__CYGWIN__) || defined(__FreeBSD__)
     return malloc_usable_size((void *)ptr);
 #else
     return 0;
