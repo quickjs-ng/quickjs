cc3e74cb00b633f4ae89601d1f9909457b7fd574
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-07-01 15:32:35 +0300

    Use brace-initialisation for JS_MKVAL under c++

--- quickjs.h
+++ quickjs.h
@@ -178,7 +178,11 @@ typedef struct JSValue {
 #define JS_VALUE_GET_FLOAT64(v) ((v).u.float64)
 #define JS_VALUE_GET_PTR(v) ((v).u.ptr)
 
+#ifdef __cplusplus
+#define JS_MKVAL(tag, val) JSValue{ { val }, tag }
+#else
 #define JS_MKVAL(tag, val) (JSValue){ (JSValueUnion){ .int32 = val }, tag }
+#endif
 #define JS_MKPTR(tag, p) (JSValue){ (JSValueUnion){ .ptr = p }, tag }
 
 #define JS_TAG_IS_FLOAT64(tag) ((unsigned)(tag) == JS_TAG_FLOAT64)
