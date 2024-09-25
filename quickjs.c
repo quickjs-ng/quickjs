1ca4761c94b7c9e94c706e8db40584a9a1d1fadd
Ben Noordhuis <info@bnoordhuis.nl>
2024-09-25 21:59:50 +0200

    squash! fix Iterator constructor

--- quickjs.c
+++ quickjs.c
@@ -39753,18 +39753,18 @@ static JSValue js_array_iterator_next(JSContext *ctx, JSValue this_val,
     }
 }
 
-static JSValue js_iterator_constructor(JSContext *ctx, JSValue this_val,
+static JSValue js_iterator_constructor(JSContext *ctx, JSValue new_target,
                                        int argc, JSValue *argv)
 {
     JSObject *p;
 
-    if (JS_TAG_OBJECT != JS_VALUE_GET_TAG(this_val))
+    if (JS_TAG_OBJECT != JS_VALUE_GET_TAG(new_target))
         return JS_ThrowTypeError(ctx, "constructor requires 'new'");
-    p = JS_VALUE_GET_OBJ(this_val);
+    p = JS_VALUE_GET_OBJ(new_target);
     if (p->class_id == JS_CLASS_C_FUNCTION)
         if (p->u.cfunc.c_function.generic == js_iterator_constructor)
             return JS_ThrowTypeError(ctx, "abstract class not constructable");
-    return js_dup(this_val);
+    return js_create_from_ctor(ctx, new_target, JS_CLASS_ITERATOR);
 }
 
 static JSValue js_iterator_from(JSContext *ctx, JSValue this_val,
