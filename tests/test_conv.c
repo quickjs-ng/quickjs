2a68c7bfc95c9e4fb58a29362bbb8de66e70add8
Ben Noordhuis <info@bnoordhuis.nl>
2025-01-05 21:07:28 +0100

    Fix -Wunterminated-string-initialization warning
    Unbreaks the Cygwin build.

--- tests/test_conv.c
+++ tests/test_conv.c
@@ -133,7 +133,12 @@ size_t i64toa_radix(char buf[minimum_length(66)], int64_t n, unsigned base);
  */
 
 /* 2 <= base <= 36 */
-char const digits36[36] = "0123456789abcdefghijklmnopqrstuvwxyz";
+char const digits36[36] = {
+    '0','1','2','3','4','5','6','7','8','9',
+    'a','b','c','d','e','f','g','h','i','j',
+    'k','l','m','n','o','p','q','r','s','t',
+    'u','v','w','x','y','z'
+};
 
 /*---- variants ----*/
 
