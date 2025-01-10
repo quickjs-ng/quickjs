0f979361f53b590b3107212732331af8fcfb8cc0
Saúl Ibarra Corretgé <s@saghul.net>
2025-01-10 12:25:21 +0100

    Disable rejected promise tracker in REPL
    Fixes: https://github.com/quickjs-ng/quickjs/issues/814

--- qjs.c
+++ qjs.c
@@ -680,6 +680,7 @@ start:
                 goto fail;
         }
         if (interactive) {
+            JS_SetHostPromiseRejectionTracker(rt, NULL, NULL);
             js_std_eval_binary(ctx, qjsc_repl, qjsc_repl_size, 0);
         }
         if (standalone || compile_file) {
