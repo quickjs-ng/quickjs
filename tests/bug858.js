9ee429d11d9158366b26a8e8c8ac4df5acc9d1ed
Ammar Ahmed <ammarahmed6506@gmail.com>
2025-01-30 18:31:40 +0500

    add tests for NaN, -Infinity

--- tests/bug858.js
+++ tests/bug858.js
@@ -2,3 +2,9 @@ import {assert} from "./assert.js";
 
 Error.stackTraceLimit = Infinity;
 assert(new Error("error").stack.includes("bug858.js")); // stack should not be empty
+
+Error.stackTraceLimit = -Infinity;
+assert(new Error("error").stack.includes("bug858.js")); // stack should not be empty
+
+Error.stackTraceLimit = NaN;
+assert(new Error("error").stack.includes("bug858.js")); // stack should not be empty
