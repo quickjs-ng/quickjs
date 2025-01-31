7654928b667012311d4182a97b9007cebf399eb0
Ben Noordhuis <info@bnoordhuis.nl>
2025-01-31 11:17:39 +0100

    Unbreak tcc build
    The version of tcc in CI merely warned on unrecognized pragmas but newer
    versions treat them as fatal errors.
    
    Upgrade tcc to prevent that from happening again.

--- quickjs-libc.c
+++ quickjs-libc.c
@@ -190,8 +190,10 @@ static void js_set_thread_state(JSRuntime *rt, JSThreadState *ts)
     js_std_cmd(/*SetOpaque*/1, rt, ts);
 }
 
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wformat-nonliteral"
+#endif // __GNUC__
 static JSValue js_printf_internal(JSContext *ctx,
                                   int argc, JSValue *argv, FILE *fp)
 {
@@ -407,7 +409,9 @@ fail:
     dbuf_free(&dbuf);
     return JS_EXCEPTION;
 }
+#ifdef __GNUC__
 #pragma GCC diagnostic pop // ignored "-Wformat-nonliteral"
+#endif // __GNUC__
 
 uint8_t *js_load_file(JSContext *ctx, size_t *pbuf_len, const char *filename)
 {
