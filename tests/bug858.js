bb14020453eecec8f4f0290019a5105c7b2a0c9c
Saúl Ibarra Corretgé <s@saghul.net>
2025-02-03 11:44:18 +0100

    Refactor Error.stackTraceLimit
    - Store the JS value, so the conversion happens when the backtrace is needed
    - Prevent recursion in build_backtrace
    - Simplify code for saving / restoring exception in build_backtrace

--- tests/bug858.js
+++ tests/bug858.js
@@ -1,10 +1,26 @@
 import {assert} from "./assert.js";
 
 Error.stackTraceLimit = Infinity;
+assert(Error.stackTraceLimit === Infinity);
 assert(new Error("error").stack.includes("bug858.js")); // stack should not be empty
 
 Error.stackTraceLimit = -Infinity;
+assert(Error.stackTraceLimit === -Infinity);
 assert(!new Error("error").stack.includes("bug858.js")); // stack should be empty
 
 Error.stackTraceLimit = NaN;
+assert(Number.isNaN(Error.stackTraceLimit));
 assert(!new Error("error").stack.includes("bug858.js")); // stack should be empty
+
+Error.stackTraceLimit = -0;
+assert(Object.is(Error.stackTraceLimit, -0));
+assert(!new Error("error").stack.includes("bug858.js")); // stack should be empty
+
+const obj = { valueOf() { throw "evil" } };
+Error.stackTraceLimit = obj;
+assert(Error.stackTraceLimit === obj);
+try {
+  throw new Error("fail")
+} catch (e) {
+  assert(e.message.includes("fail"));
+}
