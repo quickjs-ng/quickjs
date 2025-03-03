454e9f36a926fe30c3ea9755f2398c834e08bf66
Ben Noordhuis <info@bnoordhuis.nl>
2025-03-03 23:30:51 +0100

    Support reading script source from stdin in qjs
    Replace js_load_file with something that doesn't choke on special files
    and don't call realpath(3) on /dev files because that doesn't work.
    
    Fixes: https://github.com/quickjs-ng/quickjs/issues/945

--- quickjs-libc.c
+++ quickjs-libc.c
@@ -418,45 +418,37 @@ fail:
 uint8_t *js_load_file(JSContext *ctx, size_t *pbuf_len, const char *filename)
 {
     FILE *f;
-    uint8_t *buf;
-    size_t buf_len;
-    long lret;
+    size_t n, len;
+    uint8_t *p, *buf, tmp[8192];
 
     f = fopen(filename, "rb");
     if (!f)
         return NULL;
-    if (fseek(f, 0, SEEK_END) < 0)
-        goto fail;
-    lret = ftell(f);
-    if (lret < 0)
-        goto fail;
-    /* XXX: on Linux, ftell() return LONG_MAX for directories */
-    if (lret == LONG_MAX) {
-        errno = EISDIR;
-        goto fail;
-    }
-    buf_len = lret;
-    if (fseek(f, 0, SEEK_SET) < 0)
-        goto fail;
-    if (ctx)
-        buf = js_malloc(ctx, buf_len + 1);
-    else
-        buf = malloc(buf_len + 1);
-    if (!buf)
-        goto fail;
-    if (fread(buf, 1, buf_len, f) != buf_len) {
-        errno = EIO;
-        if (ctx)
-            js_free(ctx, buf);
-        else
-            free(buf);
-    fail:
-        fclose(f);
-        return NULL;
-    }
-    buf[buf_len] = '\0';
+    buf = NULL;
+    len = 0;
+    do {
+        n = fread(tmp, 1, sizeof(tmp), f);
+        if (ctx) {
+            p = js_realloc(ctx, buf, len + n + 1);
+        } else {
+            p = realloc(buf, len + n + 1);
+        }
+        if (!p) {
+            if (ctx) {
+                js_free(ctx, buf);
+            } else {
+                free(buf);
+            }
+            fclose(f);
+            return NULL;
+        }
+        memcpy(&p[len], tmp, n);
+        buf = p;
+        len += n;
+        buf[len] = '\0';
+    } while (n == sizeof(tmp));
     fclose(f);
-    *pbuf_len = buf_len;
+    *pbuf_len = len;
     return buf;
 }
 
