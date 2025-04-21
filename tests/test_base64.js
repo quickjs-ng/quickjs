d83566f9b911a879a014de369eae091c2234492b
Graham Abbott <graham.abbott@gmail.com>
2025-04-20 20:13:25 -0700

    Issue #16 references not having atob and btoa (enc/dec base64)
    I've started with naive implementation, there is significant
    discussion about how to make this faster:
    
    https://stackoverflow.com/questions/342409/
    
    If there is interest I rewrite with some of these tricks.

---
+++ tests/test_base64.js
@@ -0,0 +1,37 @@
+import { assert, assertArrayEquals, assertThrows } from "./assert.js";
+
+let source_string = "QuickJS! //+!@#$%^&*()";
+
+// created with node.  
+let target_string = "UXVpY2tKUyEgLy8rIUAjJCVeJiooKQ==";
+
+function test_atob() {
+  let encoded_string = btoa(source_string);
+  assert(encoded_string == target_string);
+}
+
+function test_btoa() {
+  let decoded_string = atob(target_string);
+  assert(decoded_string == source_string);
+}
+
+function test_btoatob() {
+  assert(source_string == atob(btoa(source_string)));
+}
+
+function test_atobtoa() {
+  assert(target_string == btoa(atob(target_string)));
+}
+
+function test_fromarray() {
+  const binaryData = new Uint8Array([72, 101, 108, 108, 111]); // 'Hello'
+  const text = String.fromCharCode.apply(null, binaryData);
+  const targetText = "SGVsbG8=";
+  assert(btoa(text) == targetText);
+}
+
+test_atob();
+test_btoa();
+test_btoatob();
+test_atobtoa();
+test_fromarray();
