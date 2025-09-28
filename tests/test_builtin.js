f06d923133d3484d473ec8d3611e65972a51df56
bptato <nincsnevem662@gmail.com>
2025-09-28 12:57:00 +0200

    Sync test_builtin.js

--- tests/test_builtin.js
+++ tests/test_builtin.js
@@ -392,9 +392,9 @@ function test_math()
     assert(Math.imul((-2)**31, (-2)**31), 0);
     assert(Math.imul(2**31-1, 2**31-1), 1);
     assert(Math.fround(0.1), 0.10000000149011612);
-    assert(Math.hypot() == 0);
-    assert(Math.hypot(-2) == 2);
-    assert(Math.hypot(3, 4) == 5);
+    assert(Math.hypot(), 0);
+    assert(Math.hypot(-2), 2);
+    assert(Math.hypot(3, 4), 5);
     assert(Math.abs(Math.hypot(3, 4, 5) - 7.0710678118654755) <= 1e-15);
 }
 
