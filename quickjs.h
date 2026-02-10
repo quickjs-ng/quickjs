96bc02582eaa2bc96705ecc0ca8a89723348e165
Saúl Ibarra Corretgé <s@saghul.net>
2026-02-10 09:00:45 +0100

    Set version to 0.12.0

--- quickjs.h
+++ quickjs.h
@@ -1389,7 +1389,7 @@ JS_EXTERN int JS_SetModuleExportList(JSContext *ctx, JSModuleDef *m,
 /* Version */
 
 #define QJS_VERSION_MAJOR 0
-#define QJS_VERSION_MINOR 11
+#define QJS_VERSION_MINOR 12
 #define QJS_VERSION_PATCH 0
 #define QJS_VERSION_SUFFIX ""
 
