10b99693cdc3f37cdf05114bc2d5225de495c4a3
Saúl Ibarra Corretgé <s@saghul.net>
2026-07-31 15:23:33 +0200

    Set version to 0.16.0

--- quickjs.h
+++ quickjs.h
@@ -1448,8 +1448,8 @@ JS_EXTERN int JS_SetModuleExportList(JSContext *ctx, JSModuleDef *m,
 /* Version */
 
 #define QJS_VERSION_MAJOR 0
-#define QJS_VERSION_MINOR 15
-#define QJS_VERSION_PATCH 1
+#define QJS_VERSION_MINOR 16
+#define QJS_VERSION_PATCH 0
 #define QJS_VERSION_SUFFIX ""
 
 JS_EXTERN const char* JS_GetVersion(void);
