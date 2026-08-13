0e7a5e08f8f0606d9dc63e0ec52836cf2ee41d10
uxxca1 <mdknoob@163.com>
2026-08-13 14:15:45 +0800

    add emit_source_loc_debug function at 'while' token (#11)

--- quickjs.c
+++ quickjs.c
@@ -29883,7 +29883,7 @@ static __exception int js_parse_statement_or_decl(JSParseState *s,
         {
             int label_cont, label_break;
             BlockEnv break_entry;
-
+            emit_source_loc_debug(s);
             label_cont = new_label(s);
             label_break = new_label(s);
 
