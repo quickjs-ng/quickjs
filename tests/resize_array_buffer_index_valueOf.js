ec405e53659decd18cd8db25216eb06c2660528e
Ben Noordhuis <info@bnoordhuis.nl>
2026-09-13 20:14:57 +0200

    Update tests/resize_array_buffer_index_valueOf.js

--- tests/resize_array_buffer_index_valueOf.js
+++ tests/resize_array_buffer_index_valueOf.js
@@ -1,9 +1,9 @@
 import { assert } from "./assert.js";
-const buffer = new ArrayBuffer(4 * Int8Array.BYTES_PER_ELEMENT, {maxByteLength: 8 * Int8Array.BYTES_PER_ELEMENT,});
+const buffer = new ArrayBuffer(4, {maxByteLength: 8});
 const array = new Int8Array(buffer);
 const index = {
     valueOf() {
-        buffer.resize(6 * Int8Array.BYTES_PER_ELEMENT);
+        buffer.resize(6);
         return 4;
     },
 };
